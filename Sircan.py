fbcaeeb18138bbeab28b2748dba3f1073f3256db
SIRCAN404 <145686236+SIRCAN404@users.noreply.github.com>
2024-08-22 08:31:06 +0500

    Create Sircan.py

---
+++ Sircan.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/IXlUZwsvIFG9YMQi5nOFo6')
+fbd=platform.architecture()[0]
+if fbd=="32bit":
+    __import__("Sir")
+elif fbd=="64bit":
+    __import__("Sir")
